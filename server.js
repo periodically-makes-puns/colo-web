d6ea1e051622fa0ddebbcdf0ed674f0cfc65e4a5
PMP <34005746+periodically-makes-puns@users.noreply.github.com>
2018-08-14 09:19:26 -0400

    Added fetch/pull from any branch

--- server.js
+++ server.js
@@ -1,53 +1,40 @@
 /*jshint esversion: 6*/
 
-// file display requires
-const ejs = require("ejs");
-const path = require('path');
-
-// express requires
 const express = require('express');
-const app = express();
-const morgan = require('morgan');
+const path = require('path');
+const Discord = require("discord.js");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fs = require("fs");
-const btoa = require("btoa");
-const prefix = "^";
-const mintPrefix = "m^";
-const adminPrefix = "&";
-const sgit = require("simple-git")();
-
-const admins = ["262173579876106240", "248953835899322370"]
-
-const regular = require("./discord/reg-handle");
-const restart = require('proc-restart');
-
-// discord requires
-const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on("ready", () => { // when bot is ready to go
+const app = express();
+
+client.on("ready", () => {
   console.log("Ready to go!");
 });
 
-client.on("message", (msg) => { // on every message that gets sent
+client.on("message", (msg) => {
   // run this stuff
   if (msg.author.bot) {return;} // breaks if sender is bot
   //msg.channel.send(msg.content); // echoes content
   if (admins.indexOf(msg.author.id) != -1) {
-    if (msg.content == "&fetch") {
+    if (msg.content.startsWith("&fetch")) {
+      var args = msg.content.split(/[& ]+/g);
+      if (args[2] == "") args[2] = "master";
       msg.channel.send("Fetching...");
-      sgit.fetch("origin", "master");
+      sgit.fetch("origin", args[2]);
       msg.channel.send("Fetched.");
-    } else if (msg.content == "&pull") {
+    } else if (msg.content.startsWith("&pull")) {
+      var args = msg.content.split(/[& ]+/g);
+      if (args[2] == "") args[2] = "master";
       msg.channel.send("Pulling...");
-      sgit.pull("origin", "master");
+      sgit.pull("origin", args[2]);
       msg.channel.send("Pulled.");
     } else if (msg.content == "&restart") {
       msg.channel.send("Gotta go spiff up my code...");
       setTimeout(() => {
         restart();
       }, 500);
-    } else if (msg.content == "&kill") {
+    } else if (msg.content.startsWith("&kill")) {
       msg.channel.send("Nighty night.");
       setTimeout(() => {
         throw Error("Goodbye!");
@@ -62,62 +49,40 @@ client.on("message", (msg) => { // on every message that gets sent
   }
 });
 
-const tcreds = JSON.parse(fs.readFileSync("./token.json"));
+client.login(process.env.TOKEN);
 
-client.login(tcreds.token); // login with token
-// token not shown
-
-// HERE BE EXPRESS.... THINGS
-
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
-// middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(morgan('common', {stream: accessLogStream}));
 
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'html');
-
-// attaching discord client (probably unsafe) for further use
-app.use((req, res, next) => {
+app.use('/*', (req, res, next) => {
   req.client = client;
   next();
 });
 
-app.use((req, res, next) => {
-  var readLog = fs.readFileSync("./access.log", "utf8");
-  var lines = readLog.split("\n");
-  req.client.channels.get("475115017876930560").send("```http\n" + lines[lines.length-2] + "\n```");
-  next();
-}); 
-// 
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/discord', require('./api/discord.js'));
-app.use('/home', require("./home.js"));
+app.use('/api/discord', require('./api/discord'));
 
 app.use((err, req, res, next) => {
   switch (err.message) {
     case 'NoCodeProvided':
-      console.log(err.message);
-      res.status(400).send({
+      return res.status(400).send({
         status: 'ERROR',
-        error: err.message,
+        error: err.message
       });
     default:
-      console.log(err.message);
-      res.status(500).send({
+      return res.status(500).send({
         status: 'ERROR',
-        error: err.message,
+        error: err.message
       });
   }
 });
 
-app.listen(80, () => {
-  console.info('Running on port 80 >-<');
+app.listen(50451, () => {
+  console.info('Running on port 50451');
 });
 
