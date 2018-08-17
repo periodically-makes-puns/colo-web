e33f1d57d5258f627f4cad5746f0cba161e999b3
PMP <34005746+periodically-makes-puns@users.noreply.github.com>
2018-08-16 20:05:52 -0400

    Implemented signing up and responding via Discord.

---
+++ mtwow/mtwow.js
@@ -0,0 +1,67 @@
+/* jshint esversion: 6 */
+const fs = require("fs");
+module.exports = (client, msg) => {
+  const args = msg.content.split(/[^\w]+/g);
+  const data = fs.readFileSync("mtwow.json", "utf-8");
+  const json = JSON.parse(data);
+  // args[0] is empty, args[1] has command, args[2]+ are arguments to the command
+  switch (args[1]) {
+    case "signup":
+      if (json.current != "signup") {
+        msg.channel.send("Sorry, but you can't sign up right now. Maybe later?");
+        setTimeout(() => {
+          // delete messages
+        }, 10000);
+      } else {
+        if (json.contestants.indexOf(msg.author.id) != -1) {
+          msg.channel.send("Sorry, but you've already signed up!");
+          setTimeout(() => {
+            // delete messages
+          }, 10000);
+        } else {
+          json.contestants.push(msg.author.id);
+          json.respCount[msg.author.id] = 1;
+          json.actualRespCount[msg.author.id] = 0;
+          json.voteCount[msg.author.id] = 0;
+          msg.channel.send("Signed up!");
+          setTimeout(() => {
+            // delete messages
+          }, 10000);
+        }
+      }
+      break;
+    case "respond":
+      if (json.current != "respond") {
+        msg.channel.send("Not time to respond yet.");
+        setTimeout(() => {
+          // delete messages
+        }, 10000);
+        break;
+      }
+      if (json.contestants.indexOf(msg.author.id) == -1) {
+        msg.channel.send("You're no contestant! Get out!");
+        setTimeout(() => {
+          // delete messages
+        }, 10000);
+        break;
+      }
+      const respNum = parseInt(args[2]);
+      if (respNum > json.respCount[msg.author.id]) {
+        msg.channel.send("You don't have that many responses!");
+        setTimeout(() => {
+          // delete messages
+        }, 10000);
+        break;
+      }
+      const response = resp.slice(3);
+      const ind = json.responses.findIndex((cur) => {
+        return cur[0] == msg.author.id && cur[1] == respNum;
+      });
+      if (ind == -1) {
+        json.responses.push([msg.author.id, respNum, response.join(" "), response.length]);
+      } else {
+        json.responses[ind] = [msg.author.id, respNum, response.join(" "), response.length];
+      }
+      break;
+  }
+};
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const client = new Discord.Client();
 const app = express();
+const mtwow = require("./mtwow/mtwow.js");
 
 client.on("ready", () => {
   console.log("Ready to go!");
@@ -15,21 +16,20 @@ client.on("ready", () => {
 client.on("message", (msg) => {
   // run this stuff
   if (msg.author.bot) {return;} // breaks if sender is bot
-  //msg.channel.send(msg.content); // echoes content
   if (admins.indexOf(msg.author.id) != -1) {
     if (msg.content.startsWith("&fetch")) {
       var args = msg.content.split(/[& ]+/g);
       if (args[2] == "") args[2] = "master";
-      msg.channel.send("Fetching...");
+      msg.channel.send("Fetching from branch " + args[2]);
       sgit.fetch("origin", args[2]);
       msg.channel.send("Fetched.");
     } else if (msg.content.startsWith("&pull")) {
       var args = msg.content.split(/[& ]+/g);
       if (args[2] == "") args[2] = "master";
-      msg.channel.send("Pulling...");
+      msg.channel.send("Pulling from branch " + args[2]);
       sgit.pull("origin", args[2]);
       msg.channel.send("Pulled.");
-    } else if (msg.content == "&restart") {
+    } else if (msg.content.startsWith("&restart")) {
       msg.channel.send("Gotta go spiff up my code...");
       setTimeout(() => {
         restart();
@@ -39,13 +39,21 @@ client.on("message", (msg) => {
       setTimeout(() => {
         throw Error("Goodbye!");
       }, 500);
-    } else if (msg.content == "&stash") {
+    } else if (msg.content.startsWith("&stash")) {
       msg.channel.send("Stashing...");
       sgit.stash();
       msg.channel.send("Stashed.");
+    } else if (msg.content.startsWith("&")) {
+      msg.channel.send("That isn't a valid function.");
     }
   } else if (msg.content.startsWith("&")) {
     msg.channel.send("You can't do that!");
+  } else if (msg.content.startsWith("^")) {
+    if (msg.content == "^") {
+      msg.channel.send("Yeah, I totally agree as well.");
+    }
+  } else if (msg.content.startsWith("m^")) {
+    mtwow(client, msg);
   }
 });
 
